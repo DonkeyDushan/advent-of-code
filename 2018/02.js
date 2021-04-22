451a829ca234e56804a2e9cf3f80baa25c4b5b15
Natálie Sedláková <natsedlakova@gmail.com>
2021-04-22 19:42:04 +0200

    2018/02 refact

--- 2018/02.js
+++ 2018/02.js
@@ -6,29 +6,21 @@ const input = fs
   .split("\r\n")
   .map((element) => element.split("").sort().join(""));
 
-console.log(input);
-
 const checkRepeated = (instructions) => {
   let doublets = 0;
   let triplets = 0;
-  instructions.forEach(element => {
-    const repeated = [...element.matchAll(/(.)\1/g)];
-    let addDoublets;
-    let addTriplets;
+  instructions.forEach((instruction) => {
+    const repeated = [...instruction.matchAll(/(.)\1/g)];
+    let addDoublets = false;
+    let addTriplets = false;
     repeated.forEach((char) => {
       const character = char[1];
-      console.log(character);
-      const re = new RegExp(`(${character})\\1{2,2}`, "g");
-      const x = re.test(element);
-      if (x && !addTriplets) {
-        addTriplets = true;
-        triplets += 1;
-      } else if (!x && !addDoublets) {
-        addDoublets = true;
-        doublets += 1;
-      }
+      const hasTriplet = instruction.includes(`${character}${character}${character}`);
+      if (hasTriplet) addTriplets = true;
+      else addDoublets = true;
     });
-    console.log(element, doublets, triplets);
+    doublets += +addDoublets;
+    triplets += +addTriplets;
   });
   return doublets * triplets;
 };
